12d017da447f5b1de5b8311fbdca7f6f90fad33e
Emanuel Barbosa <emanuellbarbosa@hotmail.com>
2023-11-02 14:32:12 -0300

    feat: "Updated data extraction logic for TJPI responses."

--- api.py
+++ api.py
@@ -1,7 +1,89 @@
-from flask import Flask
-
+from flask import Flask, request, Response
+import json
+from lxml import html
+import requests
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+def get_session_cookies_and_headers():
+    response = requests.get('https://tjpi.pje.jus.br/1g/ConsultaPublica/listView.seam')
+    cookies = response.cookies
+    jsessionid = cookies.get('JSESSIONID')
+    oauth_token_request_state = cookies.get('OAuth_Token_Request_State')
+    awsalbapp = cookies.get('AWSALBAPP-0')
+    headersdata = response.headers
+
+    return jsessionid, oauth_token_request_state, awsalbapp
+
+@app.route("/api/v1/find-process/", methods=["POST"])
+def find_process_tj():
+
+    request_data = request.get_json()
+
+    if not request_data:
+        return {
+            "error": "Nenhum dado JSON encontrado no corpo da solicitação."
+        }, 400
+
+    nome_parte = request_data.get("nomeParte")
+    numero_processo = request_data.get("numeroProcesso")
+
+    print(nome_parte, numero_processo)
+
+    if not numero_processo:
+        return {
+            "error": "O campo 'numeroProcesso' são obrigatórios."
+        }, 400
+
+    jsessionid, oauth_token_request_state, awsalbapp = get_session_cookies_and_headers()
+
+    cookies = {
+    'MO': 'P',
+    'JSESSIONID': jsessionid,
+    'AWSALBAPP-1': '_remove_',
+    'AWSALBAPP-2': '_remove_',
+    'AWSALBAPP-3': '_remove_',
+    'OAuth_Token_Request_State': oauth_token_request_state,
+    'AWSALBAPP-0': awsalbapp
+    
+    }
+
+    # headers = {
+    #     'Cookie': f'MO={cookies["MO"]}; JSESSIONID={cookies["JSESSIONID"]}; AWSALBAPP-1={cookies["AWSALBAPP-1"]}; AWSALBAPP-2={cookies["AWSALBAPP-2"]}; AWSALBAPP-3={cookies["AWSALBAPP-3"]}; OAuth_Token_Request_State={cookies["OAuth_Token_Request_State"]}; AWSALBAPP-0={cookies["AWSALBAPP-0"]}',
+        
+    # }
+
+    headers = {
+    'authority': 'tjpi.pje.jus.br',
+    'accept': '*/*',
+    'accept-language': 'pt-BR,pt;q=0.5',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    # 'cookie': 'MO=P; JSESSIONID=Ue8gSwQPQbaEcMx0N3g-L5_rs5b1Cqjk3nRwyEjX.pje-legacy-5dd9f85df9-469k6; AWSALBAPP-1=_remove_; AWSALBAPP-2=_remove_; AWSALBAPP-3=_remove_; OAuth_Token_Request_State=1c7f08a1-faf9-488d-9317-0ded07ad0a8a; AWSALBAPP-0=AAAAAAAAAACFchNmwUkTNF4dnZ9BrC/0OKd+LG9YKK+PqRsERQIWDfx6TocLHQf4Oun4aPLlIVglUHrHd4AI7mhHl+P63wJfOdpczDgmOJLotSeHYjg/EHidoriz4tWqlQfGYqhFj3c5S/Y=',
+    'origin': 'https://tjpi.pje.jus.br',
+    'referer': 'https://tjpi.pje.jus.br/1g/ConsultaPublica/listView.seam',
+    'sec-ch-ua': '"Chromium";v="118", "Brave";v="118", "Not=A?Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'sec-gpc': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+}
+
+    data = f'AJAXREQUEST=_viewRoot&fPP%3AnumProcesso-inputNumeroProcessoDecoration%3AnumProcesso-inputNumeroProcesso={numero_processo}&mascaraProcessoReferenciaRadio=on&fPP%3Aj_id147%3AprocessoReferenciaInput=&fPP%3Adnp%3AnomeParte=&fPP%3Aj_id165%3AnomeAdv=&fPP%3Aj_id174%3AclasseProcessualProcessoHidden=&fPP%3ADecoration%3AnumeroOAB=&fPP%3ADecoration%3Aj_id200=&fPP%3ADecoration%3AestadoComboOAB=org.jboss.seam.ui.NoSelectionConverter.noSelectionValue&fPP=fPP&autoScroll=&javax.faces.ViewState=j_id1&fPP%3Aj_id206=fPP%3Aj_id206&AJAX%3AEVENTS_COUNT=1&'
+
+    response = requests.post('https://tjpi.pje.jus.br/1g/ConsultaPublica/listView.seam', cookies=cookies, headers=headers, data=data)
+    
+
+    if response.status_code <= 300:
+        response_data = response.text
+        print(response_data)
+
+    return {
+        "tj_process": response_data,
+        
+    }
+
+
+
